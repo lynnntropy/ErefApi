803134466f98cff3d4fa1a91b3cb35873350442c
Veselin Romić <omegavesko@gmail.com>
2018-05-13 23:01:07 +0200

    Add CORS hadling

--- ErefREST/Startup.cs
+++ ErefREST/Startup.cs
@@ -23,6 +23,7 @@ namespace ErefREST
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc();
         }
 
@@ -34,6 +35,7 @@ namespace ErefREST
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(builder => builder.AllowAnyOrigin());
             app.UseMvc();
         }
     }
